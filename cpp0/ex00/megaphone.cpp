218043011e52836bd5064820b0f0b6b92b942fdc
gsmets <gsmets@student.42.fr>
2021-03-01 17:04:01 +0100

    changes \n to std::endl for better compatibility

--- cpp0/ex00/megaphone.cpp
+++ cpp0/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: gsmets <gsmets@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/01 15:53:21 by gsmets            #+#    #+#             */
-/*   Updated: 2021/03/01 16:00:06 by gsmets           ###   ########.fr       */
+/*   Updated: 2021/03/01 17:03:18 by gsmets           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	main(int ac, char **av)
 
 	if (ac == 1)
 	{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (0);
 	}
 	i = 0;
@@ -29,6 +29,6 @@ int	main(int ac, char **av)
 		while (av[i][j])
 			std::cout << (char)toupper(av[i][j++]);
 	}
-	std::cout << "\n";
+	std::cout << std::endl;
 	return (0);
 }
