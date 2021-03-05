96fb6de8071b548c8f1c26d966efc0944e4c1298
gsmets <gsmets@student.42.fr>
2021-03-05 18:11:51 +0100

    update to be C++98 compliant

--- cpp1/ex02/ZombieEvent.cpp
+++ cpp1/ex02/ZombieEvent.cpp
@@ -6,12 +6,13 @@
 /*   By: gsmets <gsmets@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/04 17:02:33 by gsmets            #+#    #+#             */
-/*   Updated: 2021/03/04 18:46:17 by gsmets           ###   ########.fr       */
+/*   Updated: 2021/03/05 17:22:14 by gsmets           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ZombieEvent.hpp"
 #include <cstdlib>
+#include <ctime>
 
 ZombieEvent::ZombieEvent() : _type("default") {
 	srand(time(0));
