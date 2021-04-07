b3de8b63c51049a4de2c35c99fdace263cbc5709
gsmets <gsmets@student.42.fr>
2021-04-07 19:25:37 +0200

    cpp6 ex01 done

--- cpp6/ex01/main.cpp
+++ cpp6/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gsmets <gsmets@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/07 14:59:10 by gsmets            #+#    #+#             */
-/*   Updated: 2021/04/07 15:58:46 by gsmets           ###   ########.fr       */
+/*   Updated: 2021/04/07 19:24:47 by gsmets           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,74 @@ typedef	struct 	Data {
 }					Data;
 
 void	*serialize(void) {
-	std::string	array("0123456789ABCDEF");
-	std::cout << sizeof(array) << std::endl;
-	return (NULL);
+	// Allocating 32 + 4 + 32 = 68 bytes. (If on mac, it would be 24 + 4 + 24 = 52)
+	char	*raw = new char[20];
+	char	str1[9];
+	char	str2[9];
+	int		integer;
+
+	// Filling up the two strings randomly
+	std::string	alphanum("0123456789ABCDEF");
+	for (int i = 0; i < 8; i++) {
+		str1[i] = alphanum[rand() % 16];
+		str2[i] = alphanum[rand() % 16];
+	}
+	str1[8] = '\0';
+	str2[8] = '\0';
+
+	// Random int in range 0 - 200
+	integer = rand() % 200;
+
+	//information about created values
+	std::cout << "- strings and int created -" << std::endl;
+	std::cout << "Str1 is: " << str1 << std::endl;
+	std::cout << "Int is: " << integer << std::endl;
+	std::cout << "Str2 is: " << str2 << std::endl;
+
+	// Copying the strings and integer, byte per byte in our raw serialized data
+	char *tmpraw = str1;
+	for (int i = 0; i < 8; i++)
+		raw[i] = *(tmpraw++);
+	tmpraw = reinterpret_cast<char*>(&integer);
+	for (int i = 8; i < 12; i++)
+		raw[i] = *(tmpraw++);
+	tmpraw = str2;
+	for (int i = 12; i < 20; i++)
+		raw[i] = *(tmpraw++);
+
+	return (reinterpret_cast<void*>(raw));
+}
+
+Data	*deserialize(void *raw) {
+	char *rawChar = reinterpret_cast<char*>(raw);
+	Data *ret = new Data;
+	char *tmp = new char[4];
+
+	ret->str1 = std::string(rawChar, 8);
+	ret->str2 = std::string(rawChar + 12, 8);
+
+	for (int i = 0; i < 4; i++)
+		tmp[i] = rawChar[8 + i];
+	ret->n = *(reinterpret_cast<int*>(tmp));
+
+	delete[] tmp;
+	return (ret);
 }
 
 int 	main() {
 	// Initializing rand seed (new one every second)
 	srand(time(0));
-	serialize();
+
+	void *raw = serialize();
+	Data *test = deserialize(raw);
+
+	std::cout << "----- DATA -----" << std::endl;
+	std::cout << "str1 is: " << test->str1 << std::endl;
+	std::cout << "integer is: " << test->n << std::endl;
+	std::cout << "str3 is: " << test->str2 << std::endl;
+
+	delete		test;
+	delete[]	reinterpret_cast<char*>(raw);
+
 	return (0);
 }
